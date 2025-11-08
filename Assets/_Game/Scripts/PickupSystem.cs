2746785f8ceeed3f0fcd3a923590a69f36b29d6f
0NIN0 <126891638+00NIN00@users.noreply.github.com>
2025-11-08 18:17:04 +0300

    changed implementation writing money to wallet

--- Assets/_Game/Scripts/PickupSystem.cs
+++ Assets/_Game/Scripts/PickupSystem.cs
@@ -1,16 +1,29 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 namespace _Game.Scripts
 {
     public class PickupSystem : MonoBehaviour
     {
+        private Queue<int> _moneys =  new Queue<int>();
+
         private void OnTriggerEnter(Collider other)
         {
             if (other.gameObject.TryGetComponent(out PickupObject pickupObject))
             {
-                EventInterlayer.Instance.Set(pickupObject.Value);
+                _moneys.Enqueue(pickupObject.Value);
                 pickupObject.PickUp();
             }
+        }
+        
+        public bool TryGet(out int value)
+        {
+            value = 0;
+
+            if (_moneys.Count <= 0) return false;
+            
+            value = _moneys.Dequeue();
+            return true;
         }
     }
 }
--- Assets/_Game/Scripts/Wallet.cs
+++ Assets/_Game/Scripts/Wallet.cs
@@ -1,19 +1,28 @@
 using System;
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 namespace _Game.Scripts
 {
     public class Wallet : MonoBehaviour
     {
+        private PickupSystem _pickupSystem;
+        
         private int _value;
 
         public int Value => _value;
-        
+
+        private void Awake()
+        {
+            _pickupSystem = GetComponent<PickupSystem>();
+        }
+
         private void Update()
         {
-            if (EventInterlayer.Instance.TryGet(out int value))
+            if (_pickupSystem.TryGet(out int money))
             {
-                Add(value);
+                Add(money);
             }
         }
 
